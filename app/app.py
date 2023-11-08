99c784a3375d9ff40a7e8af9d6260cf55f996646
Evelin Evlogiev <eevlogiev@fuze.com>
2023-11-09 01:45:12 +0200

    updated Dockerfile

--- app/app.py
+++ app/app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Hello DevOps Gurus!"
 
 
 if __name__ == "__main__":
-    app.run(port=os.environ.get("PORT", 5001), host="0.0.0.0")
+    app.run(port=os.environ.get("PORT", 5000), host="0.0.0.0")
